79a5f5139ec59e838680ef16719456f99b92066c
salamidrus <idrussalam95gmail.com>
2021-03-01 08:42:18 +0700

    Add Spinner and Conditional Rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,37 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) =>
         this.setState({
           errorMessage: err.message,
-        });
-      }
+        })
     );
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log("My component was just updated - it rendered");
+  }
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Lat: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return (
+      <div>
+        <Spinner message="Please accept the location request!" />
+      </div>
+    );
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
